493eaa829a8b45939a0622fdbf039bd50f327e1e
Евгений <40506054+commeta@users.noreply.github.com>
2020-08-16 05:40:23 +0300

    Update plugin.lastmodified.php

--- core/components/lastmodified/elements/plugins/plugin.lastmodified.php
+++ core/components/lastmodified/elements/plugins/plugin.lastmodified.php
@@ -78,10 +78,6 @@ if ($modx->event->name == 'OnWebPagePrerender') {
 
     if( empty($cached_page) ){
         minify_html($output);
-        
-        if( strpos( $_SERVER['HTTP_ACCEPT'], 'image/webp' ) !== false ) { // webp is supported!
-            image_webp_replace($output);
-        }
 
         $modx->cacheManager->set($cache_key, $output, 0, $options);
     } else {
@@ -151,41 +147,6 @@ if ($modx->event->name == 'OnDocFormSave') {
 }
 
 
-
-function image_webp_replace(&$output){ // Подмена картинок в контенте, на webp
-    $imgs = [];
-    $uniq_imgs= [];
-    preg_match_all('/<img[^>]+>/i',$output, $result);
-    
-    if (count($result))	{
-    	foreach($result[0] as $img_tag)	{			
-    		preg_match('/(src)=("[^"]*")/i',$img_tag, $img[$img_tag]);						
-    		$img_real = str_replace('"','',$img[$img_tag][2]);
-    		$img_real = str_replace('./','',$img_real);			
-    
-     	 	if ((strpos($img_real, '.jpg')!==false) or (strpos($img_real, '.jpeg')!==false) or (strpos($img_real, '.png')!==false)) {
-     	 	    if( !in_array($img_real, $uniq_imgs) ){
-     	 	        $uniq_imgs[]= $img_real;
-     	 	        
-     	 	        $abs= rel2abs_img( $img_real, MODX_SITE_URL.parse_url($_SERVER['REQUEST_URI'])['path'] );
-     	 	        $abs_base= str_replace('//', '/', MODX_BASE_PATH.$abs);
-     	 	        
-     	 	        $webp= '/webp'.$abs.'.webp';
-     	 	        $webp_base= str_replace('//', '/', MODX_BASE_PATH.$webp);
-     	 	        
-     	 	        if( file_exists($abs_base) && file_exists($webp_base) ){
-     	 	            $output= str_replace($img_real, $webp, $output);
-     	 	            $imgs[]= $abs;
-     	 	        }
-     	 	    }
-     	 	}
-    	}
-    } 
-}
-
-
-
-
 function minify_html(&$output){
     //remove redundant (white-space) characters
     $replace = array(
@@ -216,42 +177,3 @@ function minify_html(&$output){
     );
     $output = preg_replace(array_keys($replace), array_values($replace), $output);
 } 
-
-
-
-function rel2abs_img( $rel, $base ) {
-	// parse base URL  and convert to local variables: $scheme, $host,  $path
-	extract( parse_url( $base ) );
-
-	if ( strpos( $rel,"//" ) === 0 ) {
-		return $scheme . ':' . $rel;
-	}
-
-	// return if already absolute URL
-	if ( parse_url( $rel, PHP_URL_SCHEME ) != '' ) {
-		return $rel;
-	}
-
-	// queries and anchors
-	if ( $rel[0] == '#' || $rel[0] == '?' ) {
-		return $base . $rel;
-	}
-
-	// remove non-directory element from path
-	$path = preg_replace( '#/[^/]*$#', '', $path );
-
-	// destroy path if relative url points to root
-	if ( $rel[0] ==  '/' ) {
-		$path = '';
-	}
-
-	// dirty absolute URL
-	$abs = $path . "/" . $rel;
-
-	// replace '//' or  '/./' or '/foo/../' with '/'
-	$abs = preg_replace( "/(\/\.?\/)/", "/", $abs );
-	$abs = preg_replace( "/\/(?!\.\.)[^\/]+\/\.\.\//", "/", $abs );
-
-	// absolute URL is ready!
-	return $abs;
-}
